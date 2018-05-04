3f55fe54213bf80484487c3a90ee8610e7c8121c
PENG <13910555745@139.com>
2018-05-04 15:29:57 +0800

    node

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
 
 
-app.use(bodyParser());
+app.use(bodyParser());//bodyparser注册到router之前
 app.use(controller());
 app.listen(3000);
 console.info("localhost:3000...");
