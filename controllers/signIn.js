4a91e76146faca2bad2f1a7f90ec454f36cc4a31
peng887 <13910555745@139.com>
2018-06-13 13:18:09 +0800

    hash token

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,10 +1,16 @@
 var signIn = async (ctx, next) => {
 	const monk = require('monk');
 	const db = monk("localhost/admin")
+	const crypto = require('crypto');
+	const secret = '29aisf928af9poir9284wqefuass'
+	const setToken = token => {
+		let hash = crypto.createHmac('sha256', secret).update(token).digest('hex')
+		return hash
+	}
 
-	let requestData = ctx.request.body;
-	let userName = requestData.userName;
-	let passWord = requestData.passWord;
+	const requestData = ctx.request.body;
+	const userName = requestData.userName;
+	const passWord = requestData.passWord;
 
 	let query = ()=>{
 		return new Promise((resolve,reject)=>{
@@ -23,17 +29,22 @@ var signIn = async (ctx, next) => {
 			message:"用户名不存在"
 		}
 	}else{
-		let dbUserName = result[0].userName;
-		let dbPassWord = result[0].passWord;
+		let dbUserId = result[0].id
+		let dbUserName = result[0].userName
+		let dbPassWord = result[0].passWord
  		if(userName==dbUserName&&passWord!=dbPassWord){
 			ctx.body={
 				success:false,
 				message:"密码错误"
 			}
 		}else	if(userName==dbUserName&&passWord==dbPassWord){
+			let n = Number(ctx.cookies.get('view') || 0) + 1;
+			ctx.cookies.set('view', n);
+			let token = setToken(dbUserId+dbUserName+n)//n也可以用时间戳代替
 	 		ctx.body={
 				success:true,
-				message:"登录成功"
+				message:"登录成功",
+				token:token
 	 		}
 	 	}
 	}
